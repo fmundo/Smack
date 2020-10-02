abc587dbb7893905e6e23647b96f687e7bcbd2a9
f.mundo <f.mundo@outlook.it>
2020-10-02 11:13:07 +0200

    login created

---
+++ app/src/main/java/com/example/smack/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.smack
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    fun onCreateUserClicked(view: View) {
+
+    }
+    fun onLoginClicked(view: View) {
+
+    }
+}
